35065afa15db4607f29a54f8a7bce1304e6b302f
liamcrumm <liamcrumm@gmail.com>
2016-09-08 15:36:38 -0400

    Edited AntLabMain.java for Person5

--- src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
+++ src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
@@ -1,8 +1,11 @@
 package edu.gatech.oad.antlab.pkg1;
 
 
+
 import edu.cs2335.antlab.pkg3.*;
 import edu.gatech.oad.antlab.person.*;
+import edu.gatech.oad.antlab.pkg1.AntLab11;
+import edu.gatech.oad.antlab.pkg1.AntLab12;
 import edu.gatech.oad.antlab.pkg2.*;
 
 
@@ -73,8 +76,8 @@ import edu.gatech.oad.antlab.pkg2.*;
                   toPrint += p4.toString("gburdell4");
                   //Person5 replace P4 with your name
                   //and gburdell5 with your gt id
-                  Person5 p5 = new Person5("P5");
-                  toPrint += p5.toString("gburdell5");
+                  Person5 p5 = new Person5("Liam Crumm");
+                  toPrint += p5.toString("lcrumm3");
 		  
         System.out.println(toPrint);
         
